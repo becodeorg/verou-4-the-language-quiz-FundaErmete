111260f81a5192832c2fd86d4510527605d0f695
FundaErmete <funda.ermete@gmail.com>
2024-01-12 15:02:57 +0100

    Add functions for checking options

--- The Language Quiz/Classes/languagegame.php
+++ The Language Quiz/Classes/languagegame.php
@@ -22,13 +22,24 @@ class LanguageGame
     public function run(): void
     {
         // TODO: check for option A or B
-
         // Option A: user visits site first time (or wants a new word)
         // TODO: select a random word for the user to translate
-
         // Option B: user has just submitted an answer
+
+        if (!isset($_POST['answer'])) {
+            $this->selectRandomWord();
+        } else {
+            $userAnswer = $_POST['answer'];
+            $this->verifyAnswer($userAnswer);
+        }
+    }
+
+    private function selectRandomWord(): void
+    {
+        $randomIndex = array_rand($this->words);
+        $this->currentWord = $this->words[$randomIndex]; 
+    }
         // TODO: verify the answer (use the verify function in the word class) - you'll need to get the used word from the array first
         // TODO: generate a message for the user that can be shown
 
     }
-}
