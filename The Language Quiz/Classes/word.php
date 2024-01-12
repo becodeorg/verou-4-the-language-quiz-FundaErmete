abc3fefe431af8b3afbadc434eeeeae7f124efa8
FundaErmete <funda.ermete@gmail.com>
2024-01-12 13:06:26 +0100

    Create functions for answers with different casing

--- The Language Quiz/Classes/word.php
+++ The Language Quiz/Classes/word.php
@@ -12,10 +12,24 @@ class Word
     $this->englishAnswer = $englishAnswer;
    }
 
+   
+        // TODO: use this function to verify if the provided answer by the user matches the correct one
+        // Allow answers with different casing
+        // Allow answers with small typo's
+
     public function verify(string $answer): bool
     {
-        // TODO: use this function to verify if the provided answer by the user matches the correct one
-        // Bonus: allow answers with different casing (example: both bread or Bread can be correct answers, even though technically it's a different string)
-        // Bonus (hard): can you allow answers with small typo's (max one character different)?
+        return strtolower($answer) === strtolower($this->englishAnswer);  
+    }
+
+    public function getFrenchWord(): string
+    {
+        return $this->frenchWord;
+    }
+
+    public function getEnglishAnswer(): string
+    {
+        return $this->englishAnswer;
     }
 }
+?>
