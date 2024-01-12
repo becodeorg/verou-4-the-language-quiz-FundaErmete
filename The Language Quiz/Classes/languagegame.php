663479b2417790eeed5af404dfc2547814a29833
FundaErmete <funda.ermete@gmail.com>
2024-01-12 15:52:30 +0100

    Make some corrections and message function

--- The Language Quiz/Classes/languagegame.php
+++ The Language Quiz/Classes/languagegame.php
@@ -10,13 +10,17 @@ class LanguageGame
     private ?Word $currentWord = null;
     private ?Player $player = null;
 
-    public function __construct()
+    public function __construct(string $playerName)
     {
-        
-        foreach (Data::words() as $frenchTranslation => $englishTranslation) {
-            // TODO: create instances of the Word class to be added to the words array
-            $this->words[] = new Word($frenchTranslation, $englishTranslation);
-        }   
+        $this->player = new Player($playerName);
+        $this->initializeWords();
+    }
+
+    private function initializeWords(): void
+    {
+        foreach (Data::words() as $frenchWord => $englishAnswer) {
+            $this->words[] = new Word($frenchWord, $englishAnswer);
+        }
     }
 
     public function run(): void
@@ -26,10 +30,10 @@ class LanguageGame
         // TODO: select a random word for the user to translate
         // Option B: user has just submitted an answer
 
-        if (!isset($_POST['answer'])) {
+        if (!isset($_POST['user_answer'])) {
             $this->selectRandomWord();
         } else {
-            $userAnswer = $_POST['answer'];
+            $userAnswer = $_POST['user_answer'];
             $this->verifyAnswer($userAnswer);
         }
     }
@@ -42,4 +46,27 @@ class LanguageGame
         // TODO: verify the answer (use the verify function in the word class) - you'll need to get the used word from the array first
         // TODO: generate a message for the user that can be shown
 
+        private function verifyAnswer(string $userAnswer): void
+        {
+            if ($this->currentWord && $this->currentWord->verify($userAnswer)) {
+                $this->player->increaseScore();
+                $message = 'Correct! Well done!';
+            } else {
+                $message = 'Oops! That\'s not correct. Try again!';
+            }
+    
+            // Display the results in the view.php file
+            require 'view.php';
+        }
+    
+        public function getCurrentWord(): ?Word
+        {
+            return $this->currentWord;
+        }
+    
+        public function getPlayer(): ?Player
+        {
+            return $this->player;
+        }
     }
+?>
