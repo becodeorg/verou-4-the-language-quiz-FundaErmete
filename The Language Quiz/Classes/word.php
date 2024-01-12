60b9936503b0f7d4af2a4f8101770721f0b084ee
FundaErmete <funda.ermete@gmail.com>
2024-01-12 12:29:44 +0100

    Add a constructor for word and answer

--- The Language Quiz/Classes/word.php
+++ The Language Quiz/Classes/word.php
@@ -3,6 +3,14 @@
 class Word
 {
     // TODO: add word (FR) and answer (EN) - (via constructor or not? why?)
+   private string $frenchword;
+   private string $englishAnswer;
+
+   public function __construct(string $frenchWord, string $englishAnswer)
+   {
+    $this->frenchWord = $frenchWord;
+    $this->englishAnswer = $englishAnswer;
+   }
 
     public function verify(string $answer): bool
     {
