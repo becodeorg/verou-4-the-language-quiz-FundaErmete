d3bcbcb41b7b6651f4ec29c4fbfc4e7517fc2747
FundaErmete <funda.ermete@gmail.com>
2024-01-15 14:47:00 +0100

    Add player name

--- The Language Quiz/index.php
+++ The Language Quiz/index.php
@@ -1,23 +1,23 @@
 <?php
 
-// Require the correct variable type to be used (no auto-converting)
+// Require the correct variable type to be used 
 declare(strict_types = 1);
 
-// Show errors so we get helpful information
+// Show errors 
 ini_set('display_errors', '1');
 ini_set('display_startup_errors', '1');
 error_reporting(E_ALL);
 
-// Load your classes
+// Load classes
 require_once 'classes/Data.php';
 require_once 'classes/LanguageGame.php';
 // require_once 'classes/Player.php'; // Only needed for extra's
 require_once 'classes/Word.php';
 
 // Start the game
-// Don't change anything in this file
-// The LanguageGame class will be your starting point
-$game = new LanguageGame();
+
+$playerName = "Funda";
+$game = new LanguageGame($playerName);
 $game->run();
 
-require 'view.php';
+require 'View.php';
