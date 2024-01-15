fd9e0c2e3a290e753070275d17df7115daf13b8a
FundaErmete <funda.ermete@gmail.com>
2024-01-15 11:54:40 +0100

    Add a form for the user

--- The Language Quiz/view.php
+++ The Language Quiz/view.php
@@ -1,13 +1,21 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
 <head>
-	<meta charset="UTF-8">
-	<meta name="viewport"
-		  content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-	<meta http-equiv="X-UA-Compatible" content="ie=edge">
-	<title>Game</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Game</title>
 </head>
 <body>
-	<!-- TODO: add a form for the user to play the game -->
+    <h1>Translate the word: <?php echo $game->getCurrentWord()->getFrenchWord(); ?></h1>
+
+    <form method="post" action="">
+        <label for="user_answer">Your answer:</label>
+        <input type="text" id="user_answer" name="user_answer" required>
+        <button type="submit">Submit</button>
+    </form>
+
+    <p><?php echo $message; ?></p>
+    <p>Player: <?php echo $game->getPlayer()->getName(); ?> | Score: <?php echo $game->getPlayer()->getScore(); ?></p>
 </body>
 </html>
